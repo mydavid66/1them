0e8712700d517b3e5a10e731dbaddbc150388f07
Davod noori <mydavid66@yahoo.com>
2013-07-18 18:27:46 +0430

    creat content.php

---
+++ content.php
@@ -0,0 +1,46 @@
+<div id="preloader-container">
+  <div id="container">	
+<!--start my code -->
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div class="widget portfolio graphics homepage">
+		<div class="entry-container span4">
+			<div class="widget-slider">
+				<div class="flexslider" style="height:229px">
+					<ul class="slides">
+			    		<li>
+				     		 <div class="entry-image">
+				     		 	<a href="<?php  echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" class="fancybox">
+				     		 		<span class="entry-image-overlay"></span>
+				     		 		<img src="<?php  echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" alt="" />
+				     		 	</a>
+				     		 </div>
+			    		</li>
+					</ul>
+				</div>
+			</div>
+	  				<div class="entry drop-shadow curved">
+						<!-- Portfolio Heading -->
+						<h5 class="heading">
+							<a href="<?php the_permalink() ?>">	
+								<?php the_title() ?>
+							</a>
+						</h5>
+						
+						<!-- Portfolio Description -->
+						<p><b><?php the_excerpt()?><br>
+						<?php the_content() ?><br>
+						<?php the_category() ?><br>
+						<?php the_author() ?><br>
+						<?php the_tags()?>
+						</b>
+						</p>
+		   		 	
+				   		<div class="stripes"></div>
+
+			   		</div>
+	 </div>
+</div>
+
+	<?php endwhile; // end of the loop. ?>
+</div>
+</div>
