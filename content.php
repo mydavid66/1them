edf8b105c64ae6afa1937ade87af6449586c796c
Davod noori <mydavid66@yahoo.com>
2013-07-20 12:16:19 +0430

    Update content.php

--- content.php
+++ content.php
@@ -1,5 +1,5 @@
 <div id="preloader-container">
-  <div id="container">	
+	<div id="container">	
 <!--start my code -->
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <div class="widget portfolio graphics homepage">
@@ -7,17 +7,28 @@
 			<div class="widget-slider">
 				<div class="flexslider" style="height:229px">
 					<ul class="slides">
-			    		<li>
+			    		<li><?php if (has_post_thumbnail()) { ?>
+			
 				     		 <div class="entry-image">
 				     		 	<a href="<?php  echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" class="fancybox">
 				     		 		<span class="entry-image-overlay"></span>
 				     		 		<img src="<?php  echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" alt="" />
 				     		 	</a>
 				     		 </div>
+				     		 <?php } else {
+				     		 	echo "<div class='entry-image'>
+				     		 	<a href='images/index.jpg'  class='fancybox'>
+				     		 		<span class='entry-image-overlay'></span>
+				     		 		<img src='images/index.jpg' alt='' />
+				     		 	</a>
+				     		 </div><br>";
+				     		  } ?>
+
 			    		</li>
 					</ul>
 				</div>
 			</div>
+
 	  				<div class="entry drop-shadow curved">
 						<!-- Portfolio Heading -->
 						<h5 class="heading">
