2fc53e2750a2482de307f07c123229497d47da98
Davod noori <mydavid66@yahoo.com>
2013-07-21 18:01:30 +0430

    Update content.php

--- content.php
+++ content.php
@@ -1,57 +1,74 @@
 <div id="preloader-container">
-	<div id="container">	
-<!--start my code -->
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<div class="widget portfolio graphics homepage">
-		<div class="entry-container span4">
-			<div class="widget-slider">
-				<div class="flexslider" style="height:229px">
-					<ul class="slides">
-			    		<li><?php if (has_post_thumbnail()) { ?>
-			
-				     		 <div class="entry-image">
-				     		 	<a href="<?php  echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" class="fancybox">
-				     		 		<span class="entry-image-overlay"></span>
-				     		 		<img src="<?php  echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" alt="" />
-				     		 	</a>
-				     		 </div>
-				     		 <?php } else {
-				     		 	echo "<div class='entry-image'>
-				     		 	<a href='images/index.jpg'  class='fancybox'>
-				     		 		<span class='entry-image-overlay'></span>
-				     		 		<img src='images/index.jpg' alt='' />
-				     		 	</a>
-				     		 </div><br>";
-				     		  } ?>
+<div id="container">
+<?php
 
-			    		</li>
-					</ul>
+$args = array(
+  'post_type' => 'post',
+  'orderby' => 'title',
+  'order' => 'ASC'
+);
+
+$query = new WP_Query($args);
+
+while($query->have_posts()){
+  $query->the_post();
+?>
+
+  <div class="widget portfolio web homepage">
+    <div class="entry-container span4">
+    
+      <!-- Portfolio Image -->
+      	<?php if (has_post_thumbnail()) { ?>
+		<div class="entry-image">
+			<a href="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" class="fancybox">
+			<span class="entry-image-overlay"></span>
+			<img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );?>" alt="" />
+		</a>
+		</div>
+		<?php } else {?>
+				<div class='entry-image'>
+				<a href='images/_hick.jpg' class='fancybox'>
+				<span class='entry-image-overlay'></span>
+				<img src='images/_hick.jpg' alt='' />
+				</a>
 				</div>
-			</div>
+		<?php } ?>
 
-	  				<div class="entry drop-shadow curved">
-						<!-- Portfolio Heading -->
-						<h5 class="heading">
-							<a href="<?php the_permalink() ?>">	
-								<?php the_title() ?>
-							</a>
-						</h5>
-						
-						<!-- Portfolio Description -->
-						<p><b><?php the_excerpt()?><br>
-						<?php the_content() ?><br>
-						<?php the_category() ?><br>
-						<?php the_author() ?><br>
-						<?php the_tags()?>
-						</b>
-						</p>
-		   		 	
-				   		<div class="stripes"></div>
 
-			   		</div>
-	 </div>
-</div>
+      <div class="entry drop-shadow curved ">
 
-	<?php endwhile; // end of the loop. ?>
+        <!-- Portfolio Heading -->
+        <a href="<?php the_permalink() ?>">
+			<?php the_title() ?>
+			</a>
+			</h5>
+			<!-- Portfolio Description -->
+			<p><b> <?php the_content() ?><br>
+				  <?php the_category()?><br>
+				  <?php the_author() ?><br>
+				  <?php the_tags()?><br>
+				  
+
+	</b>
+</p>
+        <div class="entry-footer">
+          <ul>
+            <li class="left">Ú¯Ø±Ø§ÙÛŒÚ©</li>
+            <li class="right no-margin"><div class="icon like"></div> 3</li>
+      
+          </ul>
+        </div>
+
+        <p></p>
+          
+        <div class="stripes"></div>
+      </div>      
+    </div>
+  </div>
+
+<?php 	}
+	
+wp_reset_postdata();?>
+
 </div>
 </div>
